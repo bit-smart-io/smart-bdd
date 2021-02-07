ef8f211f3b8929994ae193f8aec65393f4feb06e
James <mejrbayliss@gmail.com>
2021-02-07 20:53:10 +0000

    Learning test for annotations

---
+++ src/test/java/junit5/parsing/results/TestAnnotationsExtension.java
@@ -0,0 +1,26 @@
+package junit5.parsing.results;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.extension.AfterTestExecutionCallback;
+import org.junit.jupiter.api.extension.BeforeTestExecutionCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+public class TestAnnotationsExtension implements BeforeTestExecutionCallback, AfterTestExecutionCallback {
+    static boolean beforeAllCalled = false;
+
+    @BeforeAll
+    public static void beforeAll() {
+        beforeAllCalled = true;
+    }
+
+    public static boolean isBeforeAllCalled() {
+        return beforeAllCalled;
+    }
+
+    @Override
+    public void beforeTestExecution(ExtensionContext context) throws Exception {}
+
+    @Override
+    public void afterTestExecution(ExtensionContext context) throws Exception { }
+
+}
