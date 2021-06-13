0501326ef064623f27210ee5f17e999fc8bebfa5
James <mejrbayliss@gmail.com>
2021-06-13 21:09:59 +0100

    Update class under test

--- webpage/smart-bdd/src/components/HelloWorld.vue
+++ webpage/smart-bdd/src/components/HelloWorld.vue
@@ -1,11 +1,25 @@
 <template>
   <div>
+    <h4>Summary Of All Tests Example</h4>
     <p> {{ indexSummary }} </p>
+
+    <h4>All Tests Suites Example</h4>
     <ul class="no-bullets">
       <li v-for="link in indexLinks.testSuites" @click="showResults(link.file)">
         {{ link.name }}
       </li>
     </ul>
+
+    <h4>ClassUnderTest Example</h4>
+    <ul class="no-bullets">
+      <li v-for="testCase in classUnderTest.testCases">
+        <b>{{ testCase.methodName }}</b>: {{ testCase.wordify }}
+      </li>
+    </ul>
+
+    <h4>ClassUnderTest Full JSON</h4>
+    <p> {{ classUnderTest }} </p>
+
 <!--    <ul>-->
 <!--      <li v-for="file in indexLinkFiles">-->
 <!--        {{ file }}-->
@@ -16,6 +30,9 @@
 
 <script lang="ts">
 import index from "../assets/sample-data/index.json";
+import classUnderTest from "../assets/sample-data/TEST-shared.undertest.ClassUnderTest.json";
+//import axios from "axios"
+
 export default {
   name: "HelloWorld",
   computed: {
@@ -37,11 +54,31 @@ export default {
     },
     indexSummary() {
       return index.summary;
-    }
+    },
+    classUnderTest() {
+      return classUnderTest;
+    },
   },
   methods: {
     showResults(file: string) {
-      // import index from "../assets/sample-data/index.json";
+      // can't load a local file using file://
+      // const fileRelative : string =  "../assets/sample-data/" + file;
+      // const fileRelative : string =  "./" + file;
+      //
+      // // // import index from "../assets/sample-data/index.json";
+      // fetch(fileRelative)
+      // // //axios.get(fileRelative)
+      //     .then(function (response) {
+      //       // handle success
+      //       console.log(response);
+      //     })
+      //     .catch(function (error) {
+      //       // handle error
+      //       console.log(error);
+      //     })
+      //     .then(function () {
+      //       // always executed
+      //     });
       return console.log("............file: " + file);
     },
   }
