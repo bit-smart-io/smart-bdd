cdbf7f735c16e53f46a3f06beb39d3bb7241038b
James <mejrbayliss@gmail.com>
2021-06-26 22:09:12 +0100

    Remove dependencies in parent build gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,21 +10,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.thoughtworks.qdox:qdox:2.0.0")
-    //implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.12.1")
-    //implementation("com.github.javaparser:javaparser-core:3.18.0")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.12.1")
-    implementation("com.fasterxml.jackson.core:jackson-annotations:2.12.1")
 
-    implementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
-//    testRuntimeOnly("org.junit.vintage:junit-vintage-engine:5.7.0")
-
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.7.0")
-    testImplementation("org.junit.platform:junit-platform-runner:1.7.0")
-
-    testImplementation("org.assertj:assertj-core:3.11.1")
-    testImplementation("org.mockito:mockito-all:1.10.19")
 }
 
 tasks.test {
