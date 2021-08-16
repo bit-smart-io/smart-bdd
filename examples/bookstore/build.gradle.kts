582e224d39f6c0585ea4546f4fc67e7a4ad90602
James <mejrbayliss@gmail.com>
2021-08-16 11:24:50 +0100

    Update bookstore to use Kotlin Gradle

---
+++ examples/bookstore/build.gradle.kts
@@ -0,0 +1,22 @@
+//apply(from = "../gradle/dependencies.gradle")
+plugins {
+    `java-library`
+    id("org.springframework.boot") version "2.5.3"
+}
+
+apply(plugin = "io.spring.dependency-management")
+
+group = "io.bitsmart.bdd.example"
+version = "1.0-SNAPSHOT"
+description = "Bookstore"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation(project(":report"))
+}
