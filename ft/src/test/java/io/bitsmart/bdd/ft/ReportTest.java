f07e6d9b95c5d34774f494b182ff904fdcbd7053
James <mejrbayliss@gmail.com>
2021-10-04 22:11:43 +0100

    Minor FT refactor

--- ft/src/test/java/io/bitsmart/bdd/ft/ReportTest.java
+++ ft/src/test/java/io/bitsmart/bdd/ft/ReportTest.java
@@ -22,6 +22,7 @@ import io.bitsmart.bdd.ft.report.ports.json.model.ReportIndex;
 import io.bitsmart.bdd.ft.report.ports.json.model.Status;
 import io.bitsmart.bdd.ft.report.ports.json.model.TestSuite;
 import io.bitsmart.bdd.report.junit5.launcher.TestLauncher;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -37,19 +38,40 @@ import static org.assertj.core.api.Assertions.assertThat;
  * */
 public class ReportTest {
 
+    private static TestSuite testSuite;
+
+    @BeforeAll
+    static void beforeAll() throws IOException {
+        TestLauncher.launch(ClassUnderTest.class);
+        testSuite = loadTestSuite(ClassUnderTest.class);
+    }
+
+    /**
+     * {
+     *   "links" : {
+     *     "testSuites" : [ {
+     *       "name" : "io.bitsmart.bdd.ft.ClassUnderTest",
+     *       "file" : "TEST-io.bitsmart.bdd.ft.ClassUnderTest.json"
+     *     } ]
+     *   },
+     *   "summary" : {
+     *     "passed" : 4,
+     *     "skipped" : 0,
+     *     "failed" : 0,
+     *     "aborted" : 0,
+     *     "tests" : 4
+     *   }
+     * }
+     * @throws IOException
+     */
     @Test
     void generatesIndexJson() throws IOException {
-        TestLauncher.launch(ClassUnderTest.class);
-
         ReportIndex reportIndex = loadReportIndex();
         assertThat(reportIndex).isNotNull();
     }
 
     @Test
-    void generatesClassUnderTestJson() throws IOException {
-        TestLauncher.launch(ClassUnderTest.class);
-
-        TestSuite testSuite = loadTestSuite(ClassUnderTest.class);
+    void generatesTestSuiteJson() {
         assertThat(testSuite).isNotNull();
         assertThat(testSuite.getName()).isEqualTo("io.bitsmart.bdd.ft.ClassUnderTest");
         assertThat(testSuite.getClassName()).isEqualTo("ClassUnderTest");
@@ -58,23 +80,53 @@ public class ReportTest {
         assertThat(testSuite.getSummary()).isEqualTo(aTestSuiteSummary().withTestCase(4).withPassed(4).withSkipped(0).withFailed(0).withAborted(0).build());
     }
 
+    /**
+     * {
+     *   "title" : "Class under test",
+     *   "name" : "io.bitsmart.bdd.ft.ClassUnderTest",
+     *   "className" : "ClassUnderTest",
+     *   "packageName" : "io.bitsmart.bdd.ft",
+     *   "methodNames" : [ "testMethod", "paramTest", "paramTest", "paramTest" ],
+     *   "summary" : {
+     *     "passed" : 4,
+     *     "skipped" : 0,
+     *     "failed" : 0,
+     *     "aborted" : 0,
+     *     "tests" : 4
+     *   },
+     *   "testCases" : [ {
+     *     "wordify" : "Passing assertion",
+     *     "status" : "PASSED",
+     *     "name" : "testMethod",
+     *     "methodName" : "testMethod",
+     *     "className" : "ClassUnderTest",
+     *     "packageName" : "io.bitsmart.bdd.ft"
+     *   }, {
+     *     "wordify" : "Passing assertion with value 1",
+     *     "status" : "PASSED",
+     *     "name" : "paramTest value 1",
+     *     "methodName" : "paramTest",
+     *     "className" : "ClassUnderTest",
+     *     "packageName" : "io.bitsmart.bdd.ft"
+     *   }, {
+     *     "wordify" : "Passing assertion with value 2",
+     *     "status" : "PASSED",
+     *     "name" : "paramTest value 2",
+     *     "methodName" : "paramTest",
+     *     "className" : "ClassUnderTest",
+     *     "packageName" : "io.bitsmart.bdd.ft"
+     *   }, {
+     *     "wordify" : "Passing assertion with value 3",
+     *     "status" : "PASSED",
+     *     "name" : "paramTest value 3",
+     *     "methodName" : "paramTest",
+     *     "className" : "ClassUnderTest",
+     *     "packageName" : "io.bitsmart.bdd.ft"
+     *   } ]
+     * }
+     */
     @Test
-    void verifyClassUnderTestJsonTopLevelFields() throws IOException {
-        TestLauncher.launch(ClassUnderTest.class);
-
-        TestSuite testSuite = loadTestSuite(ClassUnderTest.class);
-        assertThat(testSuite.getName()).isEqualTo("io.bitsmart.bdd.ft.ClassUnderTest");
-        assertThat(testSuite.getClassName()).isEqualTo("ClassUnderTest");
-        assertThat(testSuite.getPackageName()).isEqualTo("io.bitsmart.bdd.ft");
-        assertThat(testSuite.getMethodNames()).containsExactly("testMethod", "paramTest", "paramTest", "paramTest");
-        assertThat(testSuite.getSummary()).isEqualTo(aTestSuiteSummary().withTestCase(4).withPassed(4).withSkipped(0).withFailed(0).withAborted(0).build());
-    }
-
-    @Test
-    void verifyClassUnderTestJsonFirstTestSuite() throws IOException {
-        TestLauncher.launch(ClassUnderTest.class);
-
-        TestSuite testSuite = loadTestSuite(ClassUnderTest.class);
+    void generatesTestCaseJsonForTestMethod() {
         assertThat(testSuite.getTestCases()).contains(
             aTestCase()
                 .withClassName("ClassUnderTest")
