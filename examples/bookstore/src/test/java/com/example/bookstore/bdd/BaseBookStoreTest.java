6e40741b2cb4b1f878255437e6e48f80d4065485
James <mejrbayliss@gmail.com>
2021-10-20 22:17:47 +0100

    Books store example added uml

--- examples/bookstore/src/test/java/com/example/bookstore/bdd/BaseBookStoreTest.java
+++ examples/bookstore/src/test/java/com/example/bookstore/bdd/BaseBookStoreTest.java
@@ -25,7 +25,12 @@ import com.example.bookstore.model.IsbnBook;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+import com.github.tomakehurst.wiremock.stubbing.ServeEvent;
+import com.github.tomakehurst.wiremock.verification.LoggedRequest;
 import io.bitsmart.bdd.report.junit5.test.BaseTest;
+import io.bitsmart.bdd.report.mermaid.Message;
+import io.bitsmart.bdd.report.mermaid.SequenceDiagram;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -36,11 +41,18 @@ import org.springframework.http.ResponseEntity;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.List;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.findAll;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.getAllServeEvents;
+import static com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.putRequestedFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
+import static com.github.tomakehurst.wiremock.client.WireMock.verify;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -65,6 +77,15 @@ public class BaseBookStoreTest extends BaseTest {
         //.configureStaticDsl(true) //what does this do?
         .build();
 
+
+    @BeforeEach
+    void setUp() {
+        context().test().notes().diagram().add(new SequenceDiagram());
+        context().test().notes().diagram().get(0).addActor("User");
+        context().test().notes().diagram().get(0).addParticipant("BookStore");
+        context().test().notes().diagram().get(0).addParticipant("ISBNdb");
+    }
+
     @Override
     public void doc() {
     }
@@ -84,10 +105,18 @@ public class BaseBookStoreTest extends BaseTest {
         headers.setAccept(singletonList(MediaType.APPLICATION_JSON));
 
         String isbn = whenBuilder.build().getIsbn();
+        sequenceDiagram().addMessage(new Message("User", "BookStore", "/book/" + isbn));
         response = template.getForEntity("/book/" + isbn, String.class, headers);
+
+        List<ServeEvent> allServeEvents = getAllServeEvents();
+        allServeEvents.forEach(event -> {
+            sequenceDiagram().addMessage(new Message("BookStore", "ISBNdb", event.getRequest().getUrl()));
+            sequenceDiagram().addMessage(new Message("ISBNdb", "BookStore", event.getResponse().getBodyAsString()));
+        }) ;
     }
 
     public void then(ThenGetBookByIsbnBuilder thenBuilder) {
+        sequenceDiagram().addMessage(new Message("BookStore", "User", response.getBody()));
         assertThat(isbnBook(response.getBody())).isEqualTo(thenBuilder.build().getBook());
     }
 
