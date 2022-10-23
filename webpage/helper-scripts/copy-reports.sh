489042ad3fda09c9b7458ae98b63c525210b59a2
jrbayliss <mejrbayliss@gmail.com>
2022-10-23 10:05:55 +0100

    Remove vue from project
    Vue/modern webapp didn't work out.
    You'd probably need npm or similar installed.
    Pausing for now, will investigate NixOs and Svelt in the future.

--- webpage/helper-scripts/copy-reports.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cp -pR $TMPDIR/io.bitsmart.bdd.report/data/. $TMPDIR/io.bitsmart.bdd.report/smart-bdd/public/
-
-# TODO
-# 1. smart-bdd -> webapp
-# 2. add static-html
--- webpage/helper-scripts/init.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cp -pR ../smart-bdd/. $TMPDIR/io.bitsmart.bdd.report/smart-bdd/
-
-# TODO
-# 1. smart-bdd -> webapp
-# 2. add static-html
